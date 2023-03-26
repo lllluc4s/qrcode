c3b89f4ee0f4521df5d1293f66a83528415e7069
Lucas Rodrigues <lucasr4394@gmail.com>
2023-03-26 01:40:50 -0300

    docs: :memo: Implementing docblocks

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -8,6 +8,12 @@ use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QRCodeController extends Controller
 {
+    /**
+     * Generate QR code image URL based on user input.
+     *
+     * @param QrCodeRequest $request The user input request data.
+     * @return string The generated QR code image URL.
+     */
     private function generateQrCode(QrCodeRequest $request): string
     {
         $url = url('/read') . '?name=' . urlencode($request->input('name'));
@@ -22,12 +28,24 @@ class QRCodeController extends Controller
         return QrCode::format('png')->size(300)->generate($url);
     }
 
+    /**
+     * Create a new QR code image based on user input.
+     *
+     * @param QrCodeRequest $request The user input request data.
+     * @return string The created QR code image.
+     */
     private function createQrCode(QrCodeRequest $request): string
     {
         $qrCodeImage = $this->generateQrCode($request);
         return $qrCodeImage;
     }
 
+    /**
+     * Create a new QR code model based on user input.
+     *
+     * @param QrCodeRequest $request The user input request data.
+     * @return ModelQRCode The created QR code model.
+     */
     private function createModelQRCode(QrCodeRequest $request)
     {
         return ModelQRCode::create([
@@ -37,6 +55,12 @@ class QRCodeController extends Controller
         ]);
     }
 
+    /**
+     * Generate a new QR code based on user input, and save the generated QR code image and model data to the database.
+     *
+     * @param QrCodeRequest $request The user input request data.
+     * @return \Illuminate\Contracts\View\View The generated QR code image and model data.
+     */
     public function generate(QrCodeRequest $request)
     {
         $qrCodeImage = $this->createQrCode($request);
@@ -52,6 +76,12 @@ class QRCodeController extends Controller
             ->with('errors', $request->messages());
     }
 
+    /**
+     * Read a QR code based on input data and display the result.
+     *
+     * @param string $data The input data from the QR code.
+     * @return \Illuminate\Contracts\View\View The parsed QR code data.
+     */
     public function read($data)
     {
         $dataArray = explode(' ', $data);
