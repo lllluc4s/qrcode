7cff19b4248239bd7d207946e5f456062fdd23f9
Lucas Rodrigues <lucasr4394@gmail.com>
2023-03-27 16:01:37 -0300

    refactor: :recycle: Refactoring api.php

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/entries', [EntriesController::class, 'listEntries']);
